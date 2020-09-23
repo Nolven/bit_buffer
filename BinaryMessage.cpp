b250d86e0389d02bb0e664b5a32cae67f4dbf566
Niktia Nekrasov <nolvenon@gmail.com>
2020-09-23 06:33:43 +0300

    Insert and get functions

--- BinaryMessage.cpp
+++ BinaryMessage.cpp
@@ -15,5 +15,5 @@ BinaryMessage::BinaryMessage(size_t length) : _bitCounter(0), _data(length, 0)
 
 [[maybe_unused]] char *BinaryMessage::DataInChar()
 {
-    return reinterpret_cast<char *>(_data.data());
+    return reinterpret_cast<char *>( _data.data() );
 }
--- BinaryMessage.h
+++ BinaryMessage.h
@@ -4,6 +4,7 @@
 #include <iostream>
 #include <vector>
 #include <bitset>
+#include <cmath>
 
 class BinaryMessage
 {
@@ -12,16 +13,42 @@ public:
     ~BinaryMessage() = default;
 
     /**
-     * //TODO define
      * @tparam T Value type
      * @param value Value to write
-     * @param start Where to start writing
+     * @param writeStart Where to start writing
      * @param length Length to write
      * @param bigEndian order
      * @return bits written
      */
     template<typename T>
-    size_t Insert(T value, size_t start, size_t length, bool bigEndian = true) = delete;
+    size_t Insert(T value, size_t writeStart, size_t length)
+    {
+        if( _leftmostSetBit(value) > length - 1 )
+            std::cout << "Warning, length is too small\n";
+
+        size_t byte = writeStart / 8; ///< Byte in _data to write to
+        size_t bitsLeftInByte = 8 - writeStart % 8; ///< bits left in current byte
+
+        size_t bitsLeftToWrite = length; ///< remaining length
+        size_t valueStart = length;
+
+        while (bitsLeftToWrite > bitsLeftInByte)
+        {
+            _data[byte] |= _getBits(value, valueStart - bitsLeftInByte, bitsLeftInByte);
+
+            valueStart -= bitsLeftInByte;
+
+            writeStart += bitsLeftInByte;
+            byte = writeStart / 8;
+
+            bitsLeftToWrite -= bitsLeftInByte;
+            bitsLeftInByte = 8;
+        }
+
+        _data[byte] |= _getBits(value, valueStart - bitsLeftToWrite, bitsLeftToWrite) << (bitsLeftInByte - valueStart);
+
+        return length;
+    }
 
     /**
      * Writes value to the end of binary message
@@ -30,33 +57,11 @@ public:
      * @return number Added length
      */
     template<typename T>
-    size_t Append(T value, size_t length, bool bigEndian = true)
+    size_t Append(T value, size_t length)
     {
-        static_assert(!std::is_same<std::string, T>::value, "String are not allowed!");
-
-        size_t ret = length;
-
-        size_t byte = _bitCounter / 8;
-        size_t bitLeftInByte = 8 - _bitCounter % 8;
-
-        size_t valueStartBit = 0;
-
-        while( length > bitLeftInByte )
-        {
-            _data[byte] |= _getBits(value, bigEndian ? length - bitLeftInByte : valueStartBit, bitLeftInByte);
-
-            valueStartBit += bitLeftInByte;
-            _bitCounter += bitLeftInByte;
-            length -= bitLeftInByte;
-
-            bitLeftInByte = 8 - _bitCounter % 8;
-            byte = _bitCounter / 8;
-        }
-
-        _data[byte] |= _getBits(value, bigEndian ? bitLeftInByte - length : valueStartBit, length) << (bitLeftInByte - length);
+        size_t pos = Insert(value, _bitCounter, length);
         _bitCounter += length;
-
-        return ret;
+        return pos;
     }
 
     /**
@@ -66,11 +71,58 @@ public:
     [[maybe_unused]] char* DataInChar();
 
     /**
+     * Get's value from array
+     * @tparam T return type
+     * @param start position of the first bit
+     * @param length length of value
+     * @return value
+     */
+    template<typename T>
+    T Get(const size_t& start, size_t length)
+    {
+        size_t buff = start;
+        return Get<T>(buff, length);
+    }
+
+    /**
+     * Get's value from array
+     * @tparam T return type
+     * @param start position of the first bit
+     * @param length length of value
+     * @return value
+     */
+    template<typename T>
+    T Get(size_t& start, size_t length)
+    {
+        T number{};
+
+        size_t bitsToTake = length;
+        size_t byte = start / 8;
+
+        uint8_t bitsLeftInByte = 8u - (start % 8u);
+
+        while (bitsToTake > bitsLeftInByte)
+        {
+            number |= _getBits(_data[byte], 0, bitsLeftInByte);
+
+            start += bitsLeftInByte;
+            byte = start / 8;
+
+            bitsToTake -= bitsLeftInByte;
+            bitsLeftInByte = 8;
+            number <<= bitsToTake;
+        }
+
+        number |= _getBits(_data[byte], 8 - bitsToTake, bitsToTake);
+
+        return number;
+    }
+
+    /**
      * @return size Current size in bits
      */
     [[maybe_unused]] [[nodiscard]] size_t Size() const noexcept;
 
-private:
     /**
      * Get bits from value without shift (i.e. taken bits would be first)
      * @tparam T type
@@ -80,63 +132,43 @@ private:
      * @return
      */
     template<typename T>
-    inline static uint8_t _getBits(T value, size_t start, size_t quantity) noexcept
+    inline static T _getBits(T value, size_t start, size_t quantity) noexcept
     {
-        return (value & (((1u << quantity) - 1u) << start)) >> start;
-    }
-
-public:
-    /**
-     * Returns most significant bit index
-     * @tparam T input type
-     * @param val value to find in
-     * @return index of most significant set bit
-     */
-    template<typename T>
-    inline static size_t _mostSigSetBit(T val)
-    {
-        size_t index;
-        if( _littleEndian() )
-            index = _leftmostSetBit(val);
-        else
-            index = _rightmostSetBit(val);
-
-        return index;
+        T mask = (1u << quantity) - 1u;
+        return (value & (mask << start)) >> start;
     }
 
     /**
      * Finds index of leftmost set bit
      * @tparam T input type
      * @param value Where to search
-     * @return index (from left (for example for 00(1)00101 it would be 2)
+     * @return index (from left (for example for 00(1)00101 it would be 5)
      */
     template<typename T>
     inline static size_t _leftmostSetBit(T value) noexcept
     {
-        size_t length = sizeof(T);
+        size_t length = sizeof(T) - 1;
 
-        while ( !_getByte(value, length--) && length ){}
+        while ( !_getByte(value, length) && length )
+        {
+            --length;
+        }
 
-        return sizeof(T) - length;
+        return _leftmostSetBit(_getByte(value, length)) + length * 8;
     }
 
     /**
-     * Finds index of leftmost set bit
+     * Returns index of rightmost set bit
      * @tparam T input type
-     * @param value Where to search
-     * @return index (from left (for example for 00(1)00101 it would be 2)
+     * @param value input value
+     * @return index of rightmost set bit
      */
     template<typename T>
     inline static size_t _rightmostSetBit(T value) noexcept
     {
-        size_t length = sizeof(T);
-
-        while ( !_getByte(value, length--) && length ){}
-
-        return sizeof(T) - length;
+        return log2(value & -value);
     }
 
-public:
     /**
      * @return true for little, false for big
      */
@@ -150,7 +182,7 @@ public:
      * Return n-th byte of value;
      * Endianess-dependent
      * @tparam T input type
-     * @param value value to get byte from
+     * @param value value to get byte from, value considered to be stored as big-endian
      * @param n number of byte to get
      * @return byte value
      */
@@ -186,5 +218,15 @@ public:
     std::vector<uint8_t> _data; ///< Actual data //TODO don't actually need vector, can be changed to uint8_t*
     size_t _bitCounter; ///< Last written bit
 };
+
+template<>
+inline size_t BinaryMessage::_leftmostSetBit<uint8_t>(uint8_t value) noexcept
+{
+    size_t pos = 0;
+    while (value >>= 1u)
+        pos++;
+
+    return pos;
+}
 
 #endif //RBC_GW_BINARYMESSAGE_H

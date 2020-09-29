bc288f41cd8af6297c07f79fe522e0ffb9de449c
Niktia Nekrasov <nolvenon@gmail.com>
2020-09-30 00:52:06 +0300

    Clear garbage

--- test/cmake-build-debug/googletest/googletest/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/nolv/Workspace/binary-message/test/googletest/googletest
-# Build directory: /home/nolv/Workspace/binary-message/test/cmake-build-debug/googletest/googletest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
